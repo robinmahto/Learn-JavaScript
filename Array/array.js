1ab8a89c37d712bee1f77e11d549b8da200bbb2a
robinmahto <robinmahto413@gmail.com>
2023-01-18 23:24:18 +0530

    function added

--- Array/array.js
+++ Array/array.js
@@ -61,17 +61,31 @@ console.log(areas)
 
 // reduce
 
-let num = [1,2,3];
+// let num = [1,2,3];
 
-let sum = 0;;
-for(let i = 0 ; i < num.length; i++){
-  sum = sum + num[i]
-}
+// let sum = 0;;
+// for(let i = 0 ; i < num.length; i++){
+//   sum = sum + num[i]
+// }
 
-let result = num.reduce((previousValue, currentValue)=>{
-   return previousValue + currentValue;
-})
+// let result = num.reduce((previousValue, currentValue)=>{
+//    return previousValue + currentValue;
+// })
 
-console.log(result)
+// console.log(result)
 
 // console.log(sum)
+
+// Finding elements
+
+/**
+ 1) indexOf() -> to find position of an array elements through indexOf method, this method return first occurence element that you want to find, if does not found then return -1.
+ --- this method accept two arguments first is search elements and second on fromIndex 
+--- 
+ */
+
+let arr = [11,22,33,40,50,60,40]
+
+console.log(arr.indexOf(40, -1))
+
+
---
+++ Function/index.js
@@ -0,0 +1,14 @@
+// Higher Oder Function (HOF)
+
+/*
+ A function return another function, or take a function as a arguments is called a higher order function
+
+*/
+
+// eg
+
+function sum(){
+    console.log("printed")
+}
+
+setInterval(sum, 2000)
